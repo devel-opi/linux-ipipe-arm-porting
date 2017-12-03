1715b3587b78c24d24557a152f5d37294266cf31
Philippe Gerum <rpm@xenomai.org>
2017-12-03 11:57:56 +0100

    locking: ipipe: add hard lock alternative to regular spinlocks
    Hard spinlocks manipulate the CPU interrupt mask, without affecting
    the kernel preemption state in locking/unlocking operations.
    
    This type of spinlock is useful for implementing a critical section to
    serialize concurrent accesses from both in-band and out-of-band
    contexts, i.e. from root and head stages.
    
    Hard spinlocks exclusively depend on the pre-existing arch-specific
    bits which implement regular spinlocks. They can be seen as basic
    spinlocks still affecting the CPU's interrupt state when all other
    spinlock types only deal with the virtual interrupt flag managed by
    the pipeline core - i.e. only disable interrupts for the regular
    in-band kernel activity.

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -2776,6 +2776,9 @@ static void __trace_hardirqs_on_caller(unsigned long ip)
 
 void lockdep_hardirqs_on(unsigned long ip)
 {
+	if (!ipipe_root_p)
+		return;
+
 	if (unlikely(!debug_locks || current->lockdep_recursion))
 		return;
 
@@ -2794,7 +2797,7 @@ void lockdep_hardirqs_on(unsigned long ip)
 	 * already enabled, yet we find the hardware thinks they are in fact
 	 * enabled.. someone messed up their IRQ state tracing.
 	 */
-	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled()))
+	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled() && !hard_irqs_disabled()))
 		return;
 
 	/*
@@ -2820,7 +2823,12 @@ void lockdep_hardirqs_on(unsigned long ip)
  */
 void lockdep_hardirqs_off(unsigned long ip)
 {
-	struct task_struct *curr = current;
+	struct task_struct *curr;
+
+	if (!ipipe_root_p)
+		return;
+
+	curr = current;
 
 	if (unlikely(!debug_locks || current->lockdep_recursion))
 		return;
@@ -2829,7 +2837,7 @@ void lockdep_hardirqs_off(unsigned long ip)
 	 * So we're supposed to get called after you mask local IRQs, but for
 	 * some reason the hardware doesn't quite think you did a proper job.
 	 */
-	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled()))
+	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled() && !hard_irqs_disabled()))
 		return;
 
 	if (curr->hardirqs_enabled) {
@@ -2858,7 +2866,7 @@ void trace_softirqs_on(unsigned long ip)
 	 * We fancy IRQs being disabled here, see softirq.c, avoids
 	 * funny state and nesting things.
 	 */
-	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled()))
+	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled() && !hard_irqs_disabled()))
 		return;
 
 	if (curr->softirqs_enabled) {
@@ -2897,7 +2905,7 @@ void trace_softirqs_off(unsigned long ip)
 	/*
 	 * We fancy IRQs being disabled here, see softirq.c
 	 */
-	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled()))
+	if (DEBUG_LOCKS_WARN_ON(!irqs_disabled() && !hard_irqs_disabled()))
 		return;
 
 	if (curr->softirqs_enabled) {
